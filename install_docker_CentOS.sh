20b464991ca7498e4975288b9b63814dcdf6cb10
rusakovcom <118691484+rusakovcom@users.noreply.github.com>
2024-05-10 16:40:17 +0300

    Update install_docker_CentOS.sh

--- install_docker_CentOS.sh
+++ install_docker_CentOS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # CentOS, Oracle Linux
+# wget https://raw.githubusercontent.com/rusakovcom/docker/main/install_docker_CentOS.sh
 
 # install dependencies
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
